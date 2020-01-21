638d7e8406508baba698ff5b82323b242631cb42
Eric Marty <contact@ericmarty.co>
2020-01-20 20:17:11 -0800

    Update page title and about link.

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>wo</title>
+        <title>Login - wo, Workout app.</title>
         <link href="css/styles.css" rel="stylesheet" type="text/css">
     </head>
     <body>
