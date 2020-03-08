04bf6421cb5d82e0878751be839ef334f03ea1b9
Eric Marty <contact@ericmarty.co>
2020-03-07 18:09:42 -0800

    Add timer component.

---
+++ src/components/timer.js
@@ -0,0 +1,53 @@
+/* 
+ * Timer component
+ *
+ * Maintain and display a timer that counts up from 0. 
+ *
+ * Use Utils
+ *
+ */
+var Timer = (function () {
+    var start_time;
+    var end_time;
+    var elapsed_time;
+    var element;
+    const one_second = 1000;
+
+    // Class Api.
+    function init(elem) {
+        start_time = 0;
+        end_time = 0;
+        elapsed_time = 0;
+        element = elem;
+        setInterval(updateHandler, one_second);
+    }
+
+    function start() {
+        start_time = new Date();
+    }
+
+    function stop() {
+        end_time = new Date();
+    }
+
+    // Helper functions.
+    function elapsed() {
+        var current_time = new Date();
+        var time_diff = current_time - start_time;
+        elapsed_time = Math.round(time_diff / one_second);
+
+        return elapsed_time;
+    }
+
+    // Event handlers.
+    function updateHandler() {
+        //element.innerHTML = Utils.formatTime(elapsed(), Utils.format('HOURS'), false);
+        element.innerHTML = Utils.formatTime(elapsed(), Utils.get('FLEX'), true);
+    }
+
+    return {
+        init: init,
+        start: start,
+        stop: stop
+    };
+})();
