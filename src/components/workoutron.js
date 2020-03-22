93d4678df65e0887eadb72a4f4c5f09c0c162a87
Eric Marty <contact@ericmarty.co>
2020-03-22 11:30:29 -0700

    Use Workoutron to start processing the exercises.

--- src/components/workoutron.js
+++ src/components/workoutron.js
@@ -6,21 +6,53 @@
  *
  */
 var Workoutron = (function () {
-    var $element;
-    var $exercise_list;
+    var $element
+    var $exercise_list
+    var $exercise
 
     // Class Api.
     function init(element, workout_element) {
-        $element = element;
-        $exercise_list = document.getElementById('exercise__list');
+        $element = element
+        $exercise_list = document.getElementById('exercise__list')
+        $exercise = null
     }
 
     function next() {
-        var lis = $exercise_list.getElementsByTagName('li');
-        var sel = lis[0].getElementsByTagName('select');
-        console.log(sel[0].selectedOptions[0].value);
-        //console.log(sel[0].selectedOptions[0].text);
-        lis[0].parentNode.removeChild(lis[0])
+        get()
+        display()
+    }
+
+    // Helper functions
+    function get() {
+        var list_items = $exercise_list.getElementsByTagName('li')
+        if (list_items.length === 0) {
+            $exercise = null;
+            return
+        }
+
+        var select = list_items[0].getElementsByTagName('select')[0]
+
+        $exercise = {
+            name: select.selectedOptions[0].value,
+            id: select.selectedOptions[0].getAttribute('data-id'),
+            sets: select.selectedOptions[0].getAttribute('data-sets'),
+            rest_in_seconds: 60
+        }
+
+        pop(list_items[0]);
+    }
+
+    function pop(select) {
+        select.parentNode.removeChild(select)
+    }
+
+    function display() {
+        if ($exercise === null) {
+            $element.innerHTML = "No exercise."
+            return
+        }
+
+        $element.innerHTML = $exercise.name;
     }
 
     return {
