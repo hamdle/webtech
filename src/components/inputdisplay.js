39df9cb6c4b7e0f89e97d30bd3e56f0a47d2a0fc
Eric Marty <contact@ericmarty.co>
2020-03-24 18:11:47 -0700

    Clean-up input display.
    1. Remove refactored-out 'class' variable.
    2. Add name 'class' variable.

--- src/components/inputdisplay.js
+++ src/components/inputdisplay.js
@@ -9,14 +9,14 @@
  */
 var InputDisplay = (function () {
     var $element
-    var $exercise_list
     var $exercise
+    var $name
 
     // Class Api.
-    function init(element, exercise_list) {
+    function init(element) {
         $element = element
-        $exercise_list = exercise_list
         $exercise = null
+        $name = 'input-display'
     }
 
     function next() {
@@ -36,29 +36,29 @@ var InputDisplay = (function () {
         if ($exercise === null) {
             //$element.innerHTML = "<div id='finish' class='finish'>Done</div>"
             var div_finish = document.createElement('div')
-            div_finish.id = 'workoutron__finish'
-            div_finish.className = 'workoutron__finish'
+            div_finish.id = $name + '__finish'
+            div_finish.className = $name + '__finish'
             div_finish.innerHTML = 'Done'
             $element.appendChild(div_finish)
             return
         }
 
         var div_label = document.createElement('div')
-        div_label.id = 'workoutron__exercise--label'
-        div_label.className = 'workoutron__exercise--label'
+        div_label.id = $name + '__exercise--label'
+        div_label.className = $name + '__exercise--label'
         div_label.innerHTML = $exercise.name
         $element.appendChild(div_label)
 
 
         var div_input = document.createElement('div')
-        div_input.id = 'workoutron__exercise--input'
-        div_input.className = 'workoutron__exercise--input'
+        div_input.id = $name + '__exercise--input'
+        div_input.className = $name + '__exercise--input'
         $element.appendChild(div_input)
 
 
         var div_sets = document.createElement('div')
-        div_sets.id = 'workoutron__exercise--sets'
-        div_sets.className = 'workoutron__exercise--sets'
+        div_sets.id = $name + '__exercise--sets'
+        div_sets.className = $name + '__exercise--sets'
         div_sets.innerHTML = $exercise.sets + 'x' + $exercise.id
         div_input.appendChild(div_sets)
 
