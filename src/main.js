d071ac981780542574b16df7699b0650bb5deadc
Eric Marty <contact@ericmarty.co>
2020-04-12 18:09:56 -0700

    Refactor main.js a bit.

--- src/main.js
+++ src/main.js
@@ -7,45 +7,36 @@ let site = "http://workout.local/";
 
 let routes = [ 
     '',
-    'start-a-workout/',
+    'go/',
     'stats/',
     'exercises/',
     'settings/',
-    'go/'
 ]
 
 let path = location.pathname.substring(1)
 
-routes.map(run);
-
-function run(uri, index) {
+routes.map(function (uri, index) {
     if (path == uri) {
         switch (index) {
             case 0: 
-                loadTooltips();
+                loadTooltips()
                 runIndex()
                 break
             case 1:
-                loadTooltips();
+                loadTooltips()
+                runGo()
                 break
             case 2:
-                loadTooltips();
-                break
             case 3:
-                loadTooltips();
-                break;
             case 4:
-                loadTooltips();
-                break;
-            case 5:
-                loadTooltips();
-                go();
+                loadTooltips()
         }
     }
-}
+})
 
-// go.php
-function go() {
+// /go
+function runGo() {
+    // Init components.
     Workout.init();
     RoutineBuilder.init(document.getElementById('exercise__list'));
     Startbutton.init(startHandler);
@@ -70,9 +61,10 @@ function go() {
     }*/
 }
 
-// index.php
+// /
 function runIndex() {
     window.addEventListener("load", function() {
+        // Helper functions.
         function sendData() {
             const $xhr = new XMLHttpRequest();
             const formData = new FormData(form);
@@ -98,9 +90,10 @@ function runIndex() {
             $xhr.send(formData);
         }
 
+        // Run on load.
         let form = document.getElementById('loginForm');
 
-        // Take over submit
+        // Handle login submit through Api.
         form.addEventListener("submit", function (event) {
             event.preventDefault();
 
@@ -109,43 +102,27 @@ function runIndex() {
     });
 }
 
-// /ready
-function runReady() {
-    // This request sends a simple GET request
-    function requestListener() {
-        console.log(this.responseText);
-        responseData = JSON.parse(this.responseText);
-        console.log(this.status);
-        console.log(responseData);
-        document.getElementById('program').outerHTML = 'Workout for <strong>' + responseData['user']  + '</strong>' + responseData['workout_html'];
-    }
-
-    var request = new XMLHttpRequest();
-    request.addEventListener("load", requestListener);
-    request.open("GET", siteApi + '1/workouts/new');
-    request.send();
-}
-
+// Helper functions.
 function loadTooltips() {
     window.addEventListener('load', function() {
-    const buttons = document.querySelectorAll('.menu__link');
-    for (var i = 0; i < buttons.length; i++) {
-        var btn = buttons[i];
-        /*btn.addEventListener('click', function() {
-            console.log('click');
-        });*/
-        btn.onmouseover = function() {
-            var tooltip = event.target.querySelectorAll('.menu__tooltip');
-            if (tooltip[0] !== undefined) {
-                tooltip[0].className = 'menu__tooltip--show';
-            }
-        };
-        btn.onmouseout = function() {
-            var tooltip = event.target.querySelectorAll('.menu__tooltip--show');
-            if (tooltip[0] !== undefined) {
-                tooltip[0].className = 'menu__tooltip';
-            }
-        };
-    }
+        const buttons = document.querySelectorAll('.menu__link');
+        for (var i = 0; i < buttons.length; i++) {
+            var btn = buttons[i];
+            /*btn.addEventListener('click', function() {
+                console.log('click');
+            });*/
+            btn.onmouseover = function() {
+                var tooltip = event.target.querySelectorAll('.menu__tooltip');
+                if (tooltip[0] !== undefined) {
+                    tooltip[0].className = 'menu__tooltip--show';
+                }
+            };
+            btn.onmouseout = function() {
+                var tooltip = event.target.querySelectorAll('.menu__tooltip--show');
+                if (tooltip[0] !== undefined) {
+                    tooltip[0].className = 'menu__tooltip';
+                }
+            };
+        }
     });
 }
