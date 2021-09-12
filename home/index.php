bcce5eb1a1444a0904c4764b451ea28efa73a62d
hamdle <ericmarty@protonmail.com>
2021-09-11 18:24:12 -0700

    Add home (previous workouts) page

---
+++ home/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Home - Workout app.</title>
+        <link href="../css/styles.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <?php include __DIR__.'/template.html'; ?>
+
+        <script type="text/javascript" src="../src/components/workout.js"></script>
+        <script type="text/javascript" src="../src/components/routinebuilder.js"></script>
+        <script type="text/javascript" src="../src/components/startbutton.js"></script>
+        <script type="text/javascript" src="../src/components/utilities.js"></script>
+        <script type="text/javascript" src="../src/components/timer.js"></script>
+        <script type="text/javascript" src="../src/components/countdown.js"></script>
+        <script type="text/javascript" src="../src/components/inputdisplay.js"></script>
+        <script type="text/javascript" src="../src/components/instructions.js"></script>
+        <script type="text/javascript" src="../src/components/verifyuser.js"></script>
+        <script type="text/javascript" src="../src/components/version.js"></script>
+        <script type="text/javascript" src="../src/components/log.js"></script>
+        <script type="text/javascript" src="../src/main.js"></script>
+    </body>
+</html>
--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ let routes = [
     // Public pages
     '',
     // Auth pages
+    'home/',
     'go/',
 ]
 
@@ -21,12 +22,59 @@ routes.map(function (uri, index) {
                 runIndex()
                 break
             case 1:
+                runHome()
+                break
+            case 2:
                 runGo()
         }
     }
 })
 
-// '/go'
+function runIndex() {
+    window.addEventListener("load", function() {
+        // Send login form data to Api and redirect on success
+        function sendData() {
+            const $xhr = new XMLHttpRequest();
+            const formData = new FormData(form);
+
+            // TODO: Clean this up. Don't need both of thses.
+            $xhr.addEventListener('load', function(event) {
+                //console.log(event.target.responseText);
+                //window.location = site + 'go';
+            });
+            $xhr.onreadystatechange = function() {
+                if ($xhr.readyState == XMLHttpRequest.DONE) {
+                    if (this.status == 201) {
+                        window.location = site + 'home';
+                    } else {
+                        console.log('Login failed. Response code: '+this.status);
+                    }
+                }
+            }
+            $xhr.addEventListener('error', function(event) {
+                console.log('An error occured while logging in.');
+            });
+            $xhr.open("POST", api + 'login');
+            // Specifying a header here could cause the POST data to be send
+            // incorrectly, don't set it explicitly and let the broswer generate
+            // the correct one automatically
+            $xhr.send(formData);
+        }
+
+        // Run on load.
+        let form = document.getElementById('loginForm');
+        form.addEventListener("submit", function (event) {
+            event.preventDefault();
+            sendData();
+        });
+    });
+}
+
+function runHome() {
+    VerifyUser.failure('');
+    Log.init();
+}
+
 function runGo() {
     VerifyUser.failure('');
 
@@ -56,46 +104,5 @@ function runGo() {
     var timer = document.getElementById('timer')
     timer.addEventListener('click', function () {
         Countdown.start(60);
-    });
-}
-
-// '/'
-function runIndex() {
-    window.addEventListener("load", function() {
-        // Send login form data to Api and redirect on success
-        function sendData() {
-            const $xhr = new XMLHttpRequest();
-            const formData = new FormData(form);
-
-            // TODO: Clean this up. Don't need both of thses.
-            $xhr.addEventListener('load', function(event) {
-                //console.log(event.target.responseText);
-                //window.location = site + 'go';
-            });
-            $xhr.onreadystatechange = function() {
-                if ($xhr.readyState == XMLHttpRequest.DONE) {
-                    if (this.status == 201) {
-                        window.location = site + 'go';
-                    } else {
-                        console.log('Login failed. Response code: '+this.status);
-                    }
-                }
-            }
-            $xhr.addEventListener('error', function(event) {
-                console.log('An error occured while logging in.');
-            });
-            $xhr.open("POST", api + 'login');
-            // Specifying a header here could cause the POST data to be send
-            // incorrectly, don't set it explicitly and let the broswer generate
-            // the correct one automatically
-            $xhr.send(formData);
-        }
-
-        // Run on load.
-        let form = document.getElementById('loginForm');
-        form.addEventListener("submit", function (event) {
-            event.preventDefault();
-            sendData();
-        });
     });
 }
