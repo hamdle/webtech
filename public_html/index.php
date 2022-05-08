52445ce435de01f17c7c2ba430bc99460fec8917
Eric Marty <ericmarty@protonmail.com>
2022-05-08 14:13:06 -0700

    Redirect authenticated for login page

--- public_html/index.php
+++ public_html/index.php
@@ -3,6 +3,6 @@
 require_once dirname(__DIR__,1) . "/autoload.php";
 
 $app = new \web\App();
-$app->render("Login");
+$app->redirectAuthenticated("/home")->render("Login");
 
 ?>
--- web/App.php
+++ web/App.php
@@ -41,4 +41,15 @@ class App
         }
         return $this;
     }
+
+    public function redirectAuthenticated($path) {
+        if (!array_key_exists('session', self::$obj)) {
+            self::$obj['session'] = new \Models\Session();
+        }
+        if (self::$obj['session']->verify()) {
+            header("Location: " . $_ENV['ORIGIN'] . $path);
+            exit();
+        }
+        return $this;
+    }
 }
