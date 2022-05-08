16bd2dbf5139a6463ae1c592e2a852c33bded531
Eric Marty <ericmarty@protonmail.com>
2022-05-08 13:02:06 -0700

    Update log to display all workout data

--- public_html/js/components/log.js
+++ public_html/js/components/log.js
@@ -13,21 +13,71 @@ var Log = (function() {
         log = document.getElementById("log");
         Object.values(workouts).forEach(function (entry) {
             console.log(entry);
-            var div = document.createElement("div");
-            div.classList = "log__entry";
-            div.innerHTML = "<span class=\"log__title\">" +
-                "<span class=\"fa fa-clock footer__icon\"></span>" +
-                entry.start +
-                "</ span> " +
-                "<a href='/edit/index.php?id=" +
-                entry.id +
-                "'>Edit</a>";
+
+            // Wrapper
+            var wrap = document.createElement("div");
+            wrap.classList = "log__wrap";
+
+            // Workout info: start
+            var start = document.createElement("div");
+            start.classList = "log__entry"
+            start.innerHTML = entry.start;
+            wrap.appendChild(start);
+            // Workout info: end
+            var end = document.createElement("div");
+            end.classList = "log__entry"
+            end.innerHTML = entry.end;
+            wrap.appendChild(end);
+
+
+            // Exercises
+            Object.values(entry.exercises).forEach(function (exercise) {
+                var div = document.createElement("div");
+                div.classList = "log__exercise";
+                var reps = "";
+                //console.log(Object.values(exercise.reps).length);
+                var count = 0;
+                if (exercise.reps == undefined) {
+                    reps = " ";
+                } else {
+                    Object.values(exercise.reps).forEach(function (rep) {
+                        reps = reps + rep.amount;
+                        if (count < Object.values(exercise.reps).length - 1) {
+                            reps = reps + " + ";
+                        }
+                        count++;
+                    })
+                }
+
+                div.innerHTML = exercise.exercise_type.title +
+                    " --- " + exercise.sets + " X [ "+reps+" ]" +
+                    " --- " + exercise.feedback;
+                wrap.appendChild(div);
+            });
+
+            // Workout info: feel
+            var feel = document.createElement("div");
+            feel.classList = "log__entry"
+            feel.innerHTML = entry.feel;
+            wrap.appendChild(feel);
+            // Workout info: notes
+            var notes = document.createElement("div");
+            notes.classList = "log__entry"
+            notes.innerHTML = entry.notes;
+            wrap.appendChild(notes);
+            
+            // div.innerHTML = "<span class=\"log__title\">" +
+            //     "<span class=\"fa fa-clock footer__icon\"></span>" +
+            //     entry.start +
+            //     "</ span> " +
+            //     "<a href='/edit/index.php?id=" +
+            //     entry.id +
+            //     "'>Edit</a>";
             // var button = document.createElement("a");
             // button.classList = "button";
             // button.innerHTML = "Load";
             // div.appendChild(button);
-            log.appendChild(div);
-            console.log(entry);
+            log.appendChild(wrap);
         });
     }
 
