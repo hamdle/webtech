13bd89afb65d63525295d1ad8b9307881d99bafc
Eric Jawaid Marty <ericmarty@protonmail.com>
2023-05-29 16:02:03 -0700

    Make tabs work on User Settings page

--- html/pages/user.php
+++ html/pages/user.php
@@ -14,73 +14,148 @@ $App->RenderHtml('header.php');
             <h1 class="p-heading--3">
                 Settings
             </h1>
-            <nav class="p-tabs" aria-label="Example tabs navigation">
-                <ul class="p-tabs__list u-float-right u-no-margin--bottom" role="tablist">
-                    <li class="p-tabs__item" role="presentation">
-                        <a data-tour="listing-intro" href="#listing" class="p-tabs__link" tabindex="0" role="tab" aria-selected="true">
-                            User
-                        </a>
-                    </li>
-                    <li class="p-tabs__item" role="presentation">
-                        <a href="#builds" class="p-tabs__link" tabindex="0" role="tab">
-                            Workout
-                        </a>
-                    </li>
-                    <li class="p-tabs__item" role="presentation">
-                        <a href="#releases" class="p-tabs__link" tabindex="0" role="tab">
-                            System
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    </section>
-    <div class="p-stripe is-shallow">
-        <div class="row">
-            <div class="p-card u-vertically-center">
-                <div class="card">
-                    <div class="card u-clearfix">
-                        <div class="card u-float-left">
-                            <h2><?php echo $this->User->first_name." ".$this->User->last_name; ?></h2>
+            <nav class="p-tabs">
+                <div class="p-tabs__list" role="tablist" aria-label="Juju technology">
+                    <div class="p-tabs__item">
+                        <button class="p-tabs__link" role="tab" aria-selected="true" aria-controls="user-tab" id="user">User</button>
+                    </div>
+                    <div class="p-tabs__item">
+                        <button class="p-tabs__link" role="tab" aria-selected="false" aria-controls="workout-tab" id="workout" tabindex="-1">Workout</button>
+                    </div>
+                    <div class="p-tabs__item">
+                        <button class="p-tabs__link" role="tab" aria-selected="false" aria-controls="system-tab" id="system" tabindex="-1">System</button>
+                    </div>
+                </div>
+
+                <div tabindex="0" role="tabpanel" id="user-tab" aria-labelledby="user">
+                    <div class="p-stripe is-shallow">
+                        <div class="row">
+                            <div class="p-card u-vertically-center">
+                                <div class="card">
+                                    <div class="card u-clearfix">
+                                        <div class="card u-float-left">
+                                            <h2>User Settings</h2>
+                                        </div>
+                                    </div>
+
+                                </div>
+                                <div class="card">
+                                    <div class="p-card__content">
+                                        <img class="p-card__image" alt="" src="<?php echo $_ENV['ORIGIN']; ?>/img/p-bar.png" height="150">
+                                    </div>
+                                </div>
+                                <div class="card">
+                                    <table>
+                                        <tbody>
+                                        <tr>
+                                            <th>First Name</th>
+                                            <th><?php echo $App->User->first_name; ?></th>
+                                        </tr>
+                                        <tr>
+                                            <th>Last Name</th>
+                                            <th><?php echo $App->User->last_name; ?></th>
+                                        </tr>
+                                        <tr>
+                                            <th>Email</th>
+                                            <th><?php echo $App->User->email; ?></th>
+                                        </tr>
+                                        <tr>
+                                            <th>Created On</th>
+                                            <th><?php echo is_null($App->User->created_date) ? ltrim(date("m/d/Y"), "0") : $App->User->created_date; ?></th>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <div class="card">
+                                    <p class="u-clearfix">
+                                    <div id="edit" aria-controls="modal" class="card u-float-left"><button class="p-button">Edit</button></div>
+                                    </p>
+                                </div>
+                            </div>
                         </div>
                     </div>
-
                 </div>
-                <div class="card">
-                    <div class="p-card__content">
-                        <img class="p-card__image" alt="" src="<?php echo $_ENV['ORIGIN']; ?>/img/p-bar.png" height="150">
+
+                <div tabindex="0" role="tabpanel" id="workout-tab" aria-labelledby="workout" hidden="true">
+                    <div class="p-stripe is-shallow">
+                        <div class="row">
+                            <div class="p-card u-vertically-center">
+                                <div class="card">
+                                    <div class="card u-clearfix">
+                                        <div class="card u-float-left">
+                                            <h2>Workout Settings</h2>
+                                        </div>
+                                    </div>
+
+                                </div>
+                                <div class="card">
+                                    <div class="p-card__content">
+                                        <img class="p-card__image" alt="" src="<?php echo $_ENV['ORIGIN']; ?>/img/p-bar.png" height="150">
+                                    </div>
+                                </div>
+                                <div class="card">
+                                    <table>
+                                        <tbody>
+                                        <tr>
+                                            <th>Default Reps</th>
+                                            <th>60</th>
+                                        </tr>
+                                        <tr>
+                                            <th>Default Sets</th>
+                                            <th>120</th>
+                                        </tr>
+                                        <tr>
+                                            <th>Warm Up Length</th>
+                                            <th>180</th>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div class="card">
-                    <table>
-                        <tbody>
-                        <tr>
-                            <th>First Name</th>
-                            <th><?php echo $App->User->first_name; ?></th>
-                        </tr>
-                        <tr>
-                            <th>Last Name</th>
-                            <th><?php echo $App->User->last_name; ?></th>
-                        </tr>
-                        <tr>
-                            <th>Email</th>
-                            <th><?php echo $App->User->email; ?></th>
-                        </tr>
-                        <tr>
-                            <th>Created On</th>
-                            <th><?php echo is_null($App->User->created_date) ? ltrim(date("m/d/Y"), "0") : $App->User->created_date; ?></th>
-                        </tr>
-                        </tbody>
-                    </table>
+
+                <div tabindex="0" role="tabpanel" id="system-tab" aria-labelledby="system" hidden="true">
+                    <div class="p-stripe is-shallow">
+                        <div class="row">
+                            <div class="p-card u-vertically-center">
+                                <div class="card">
+                                    <div class="card u-clearfix">
+                                        <div class="card u-float-left">
+                                            <h2>System Settings</h2>
+                                        </div>
+                                    </div>
+
+                                </div>
+                                <div class="card">
+                                    <div class="p-card__content">
+                                        <img class="p-card__image" alt="" src="<?php echo $_ENV['ORIGIN']; ?>/img/p-bar.png" height="150">
+                                    </div>
+                                </div>
+                                <div class="card">
+                                    <table>
+                                        <tbody>
+                                        <tr>
+                                            <th>Timezone</th>
+                                            <th>-8</th>
+                                        </tr>
+                                        <tr>
+                                            <th>Workout Pagination Default</th>
+                                            <th>25</th>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="card">
-                    <p class="u-clearfix">
-                        <div id="edit" aria-controls="modal" class="card u-float-left"><button class="p-button">Edit</button></div>
-                    </p>
-                </div>
-            </div>
+            </nav>
+
         </div>
-    </div>
+    </section>
+
 </div>
 
 <div class="p-modal" id="modal" style="display: none;">
@@ -118,6 +193,108 @@ $App->RenderHtml('header.php');
 </div>
 
 <?php $App->RenderHtml('footer.php'); ?>
+
+<script>
+    (function () {
+        var keys = {
+            left: 'ArrowLeft',
+            right: 'ArrowRight',
+        };
+
+        var direction = {
+            ArrowLeft: -1,
+            ArrowRight: 1,
+        };
+
+        /**
+         Attaches a number of events that each trigger
+         the reveal of the chosen tab content
+         @param {Array} tabs an array of tabs within a container
+         */
+        function attachEvents(tabs) {
+            tabs.forEach(function (tab, index) {
+                tab.addEventListener('keyup', function (e) {
+                    if (e.code === keys.left || e.code === keys.right) {
+                        switchTabOnArrowPress(e, tabs);
+                    }
+                });
+
+                tab.addEventListener('click', function (e) {
+                    e.preventDefault();
+                    setActiveTab(tab, tabs);
+                });
+
+                tab.addEventListener('focus', function () {
+                    setActiveTab(tab, tabs);
+                });
+
+                tab.index = index;
+            });
+        }
+
+        /**
+         Determine which tab to show when an arrow key is pressed
+         @param {KeyboardEvent} event
+         @param {Array} tabs an array of tabs within a container
+         */
+        function switchTabOnArrowPress(event, tabs) {
+            var pressed = event.code;
+
+            if (direction[pressed]) {
+                var target = event.target;
+                if (target.index !== undefined) {
+                    if (tabs[target.index + direction[pressed]]) {
+                        tabs[target.index + direction[pressed]].focus();
+                    } else if (pressed === keys.left) {
+                        tabs[tabs.length - 1].focus();
+                    } else if (pressed === keys.right) {
+                        tabs[0].focus();
+                    }
+                }
+            }
+        }
+
+        /**
+         Cycles through an array of tab elements and ensures
+         only the target tab and its content are selected
+         @param {HTMLElement} tab the tab whose content will be shown
+         @param {Array} tabs an array of tabs within a container
+         */
+        function setActiveTab(tab, tabs) {
+            tabs.forEach(function (tabElement) {
+                var tabContent = document.getElementById(tabElement.getAttribute('aria-controls'));
+
+                if (tabElement === tab) {
+                    tabElement.setAttribute('aria-selected', true);
+                    tabContent.removeAttribute('hidden');
+                } else {
+                    tabElement.setAttribute('aria-selected', false);
+                    tabContent.setAttribute('hidden', true);
+                }
+            });
+        }
+
+        /**
+         Attaches events to tab links within a given parent element,
+         and sets the active tab if the current hash matches the id
+         of an element controlled by a tab link
+         @param {String} selector class name of the element
+         containing the tabs we want to attach events to
+         */
+        function initTabs(selector) {
+            var tabContainers = [].slice.call(document.querySelectorAll(selector));
+
+            tabContainers.forEach(function (tabContainer) {
+                var tabs = [].slice.call(tabContainer.querySelectorAll('[aria-controls]'));
+                attachEvents(tabs);
+            });
+        }
+
+        document.addEventListener('DOMContentLoaded', function () {
+            initTabs('[role="tablist"]');
+        });
+    })();
+</script>
 
 <script>
     // This is an example modal implementation inspired by
