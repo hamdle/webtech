2114942cea0fa4e6f7738b1fd65507e27a221f4c
hamdle <ericmarty@protonmail.com>
2023-12-05 14:06:24 -0600

    Update list of REST endpoints to replace with rpc calls

--- public_html/api/index.php
+++ public_html/api/index.php
@@ -7,11 +7,6 @@ require_once dirname(__DIR__, 2) . "/autoload.php";
 // TODO: endpoints to replace
 //Api::get("auth",        "Authentication",   "verifySession");
 //Api::get("exercises",   "Workouts",         "exerciseTypes");
-//Api::get("workouts",    "Workouts",         "allWorkouts");
-//Api::get("version",     "AppInfo",          "version");
-//Api::get("coffee",      "AppInfo",          "teapot");
-//Api::get("roll",        "Dice",             "d20");
-//Api::get("september",   "September",        "countdown");
 //Api::get("suggest/reps/{exercise_type_id}", "Workouts", "suggestReps");
 //Api::post("workouts/new",   "Workouts",         "save");
 
