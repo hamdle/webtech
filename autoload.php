bb9ce99b9c2011d0265e0dbf6e0630bc997c278f
hamdle <ericmarty@protonmail.com>
2023-12-04 18:23:25 -0600

    Simplify autoloader

--- autoload.php
+++ autoload.php
@@ -1,45 +1,34 @@
 <?php
 
 /*
- * autoload.php: autoload files
+ * Autoload
  *
- * Autoload files based on their location in the page folder. To use a namespace
- * of \Core\Html\Request create a directory called Core and put a directory
- * called Html inside of it. Add file Request.php into the Html directory and
- * define the Request class in it.
+ * Autoload files based on their location from the root directory.
  *
- * Copyright (C) 2021 Eric Marty
+ * @author Eric Marty
+ * @since 12/4/2023
  */
 
-require_once __DIR__ . "/api/Core/Utils/Env.php";
+use api\Core\Utils\Env;
 
-use Core\Utils\Env;
+require_once __DIR__ . "/api/Core/Utils/Env.php";
 
 class Autoload {
     public static function register()
     {
         Env::load();
-        spl_autoload_register("Autoload::loadServerFile");
+        spl_autoload_register("Autoload::loadClassFile");
     }
 
-    // $class = string (like "Core\Html\Request")
-    // return = true if found | false if not
-    public static function loadServerFile($class)
+    // $class = string like "api\Core\Html\Request"
+    public static function loadClassFile($class)
     {
-        $apiFile = __DIR__.DIRECTORY_SEPARATOR
-            ."api".DIRECTORY_SEPARATOR
-            .str_replace("\\", DIRECTORY_SEPARATOR, $class).".php";
-        $serverFile = __DIR__.DIRECTORY_SEPARATOR.
-            str_replace("\\", DIRECTORY_SEPARATOR, $class).".php";
-
-        if (file_exists($apiFile)) {
-            require_once $apiFile;
-            return true;
-        } else if (file_exists($serverFile)) {
-            require_once $serverFile;
+        $file = __DIR__."/". str_replace("\\", "/", $class).".php";
+        if (file_exists($file))
+        {
+            require_once $file;
             return true;
         }
-
         return false;
     }
 }
