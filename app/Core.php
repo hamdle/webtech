b917e2be9b62602fe8c9e9d636c8e5899536dd16
hamdle <ericmarty@protonmail.com>
2023-12-04 19:15:25 -0600

    Consistent quotes in Core.php

--- app/Core.php
+++ app/Core.php
@@ -8,10 +8,10 @@ use api\Models\Session;
 
 class Core
 {
-    public const HTML_CLOSE = 'close';
-    public const HTML_FOOTER = 'footer';
-    public const HTML_HEADER = 'header';
-    public const HTML_OPEN = 'open';
+    public const HTML_CLOSE = "close";
+    public const HTML_FOOTER = "footer";
+    public const HTML_HEADER = "header";
+    public const HTML_OPEN = "open";
     public readonly Session $session;
 
     private $name;
@@ -22,7 +22,7 @@ class Core
         $this->name = $name;
     }
 
-    public function authOrDie($message = 'Authentication error')
+    public function authOrDie($message = "Authentication error")
     {
         if (!$this->session->authenticated())
         {
@@ -33,7 +33,7 @@ class Core
 
     public function renderHtml($file)
     {
-        $filepath = dirname(__DIR__, 1) . $_ENV["HTML_DIR"] . $file . '.php';
+        $filepath = dirname(__DIR__, 1) . $_ENV["HTML_DIR"] . $file . ".php";
         if (file_exists($filepath))
         {
             require $filepath;
