e80d99a6b6957e89616275f79fc803e982df47ca
hamdle <ericmarty@protonmail.com>
2023-12-06 21:09:12 -0600

    Update list of endpoints to replace

--- public_html/api/index.php
+++ public_html/api/index.php
@@ -7,7 +7,6 @@ require_once dirname(__DIR__, 2) . "/autoload.php";
 
 // TODO: endpoints to replace
 //Api::get("auth",        "Authentication",   "verifySession");
-//Api::get("exercises",   "Workouts",         "exerciseTypes");
 //Api::get("suggest/reps/{exercise_type_id}", "Workouts", "suggestReps");
 //Api::post("workouts/new",   "Workouts",         "save");
 
