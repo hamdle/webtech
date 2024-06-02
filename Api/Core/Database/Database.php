72e6bc12f55dfc928499c9a4539563c464e1575e
Eric Jawaid Marty <ericmarty@protonmail.com>
2024-06-02 17:40:33 -0500

    Add port to database connection

--- Api/Core/Database/Database.php
+++ Api/Core/Database/Database.php
@@ -12,7 +12,7 @@ class Database {
         if (is_null(self::$db)) {
             try
             {
-                $dsn = "mysql:host=".getenv("DB_HOST").";dbname=".getenv("DB_NAME").";charset=utf8mb4";
+                $dsn = "mysql:host=".getenv("DB_HOST").";port=".getenv("DB_PORT").";dbname=".getenv("DB_NAME").";charset=utf8mb4";
                 $options = [
                     \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
                     \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
