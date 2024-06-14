cd9631c58d246dfe15d8a038dded77b22847b4a2
Eric Jawaid Marty <ericmarty@protonmail.com>
2024-06-13 20:53:08 -0500

    Implement json template renderer

--- Api/Controller/PageController.php
+++ Api/Controller/PageController.php
@@ -8,8 +8,13 @@ class PageController extends BaseController
     {
         $this->response->setHtml();
 
-        $content = $this->renderer->render(dirname(__DIR__,1) . '/Html/Page/index.php');
-        //$this->renderer->renderTemplate('page', 'test');
+        //$content = $this->renderer->render(dirname(__DIR__,1) . '/Html/Page/index.php');
+
+        $content = $this->renderer->renderTemplate(
+            'Page.json',
+            ['page' => 'Test']
+        );
+
         $this->response->setContent($content);
 
         return $this->response;
--- Api/Core/Renderer.php
+++ Api/Core/Renderer.php
@@ -2,8 +2,14 @@
 
 namespace Api\Core;
 
+use Api\Html\Template;
+
 class Renderer
 {
+    private array $template;
+
+    private bool $error = false;
+
     public function render($file)
     {
         try {
@@ -13,9 +19,68 @@ class Renderer
             ob_end_clean();
         } catch (\Throwable $th) {
             ob_end_clean();
+            $this->error = true;
             $content = '<h1>Error Loading Page</h1><p>'.$th->getMessage().' '.$th->getFile().' '.$th->getLine().'</p>';
             //throw new \Exception("Error loading page ");
         }
         return $content;
+    }
+
+    public function renderTemplate($template, $args)
+    {
+        $renderArray = [];
+        $this->error = false;
+
+        if ($this->loadTemplate(dirname(__DIR__,1) . '/Html/Template/' . $template))
+        {
+            foreach ($this->template as $file)
+            {
+                $resolvedFile = $file;
+                foreach ($args as $key => $value)
+                {
+                    $resolvedFile = str_replace('{' . $key . '}', $value, $resolvedFile);
+                }
+
+                $content = $this->render(dirname(__DIR__,1) . '/Html/'.$resolvedFile);
+
+                if ($this->error)
+                {
+                    $renderArray = [];
+                    $renderArray[] = $content;
+                    break;
+                }
+
+                $renderArray[] = $content;
+            }
+        }
+        else
+        {
+            return 'Render template failed';
+        }
+
+        return implode("\n", $renderArray);
+    }
+
+    private function loadTemplate($template): bool
+    {
+        try {
+            $content = file_get_contents($template);
+            $this->template = json_decode($content, true);
+        } catch (\Exception $e)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    public static function onPage()
+    {
+        return false;
+    }
+
+    public static function isAuthenticated()
+    {
+        return false;
     }
 }
