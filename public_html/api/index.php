fd7fc6097f638e85f2ee528512b3c37dfbe3a746
Eric Jawaid Marty <ericmarty@protonmail.com>
2024-06-15 12:06:36 -0500

    Remove outdated comments

--- public_html/api/index.php
+++ public_html/api/index.php
@@ -5,10 +5,6 @@ use Api\Core\Http\Request;
 
 require_once dirname(__DIR__, 2) . "/autoload.php";
 
-// TODO: endpoints to replace
-//Api::get("suggest/reps/{exercise_type_id}", "Workouts", "suggestReps");
-//Api::post("workouts/new",   "Workouts",         "save");
-
 $request = Request::post();
 
 $router = new Router();
