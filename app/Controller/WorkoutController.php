a6421768691324bdd93395e997edcd0fb6856fc4
Eric Jawaid Marty <ericmarty@protonmail.com>
2024-08-10 12:46:19 -0500

    Fix bug in all workout sort code to display newest first

--- app/Controller/WorkoutController.php
+++ app/Controller/WorkoutController.php
@@ -168,12 +168,12 @@ class WorkoutController extends BaseController
 
 
         $data = [];
-        $sort = 0;
+        $sort = count($workouts);
         foreach ($workouts as $workout)
         {
             $data[$workout["id"]] = $workout;
             $data[$workout["id"]]['sort'] = $sort;
-            $sort++;
+            $sort--;
         }
 
         foreach ($exercises as $exercise)
