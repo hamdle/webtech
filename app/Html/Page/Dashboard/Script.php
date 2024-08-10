4a90478b556e86b23a8d2ce03741748aec391279
Eric Jawaid Marty <ericmarty@protonmail.com>
2024-08-09 20:24:35 -0500

    Remove workout log script from dashboard page

--- app/Html/Page/Dashboard/Script.php
+++ app/Html/Page/Dashboard/Script.php
@@ -1,10 +1,3 @@
-<script src="<?php echo $_ENV['ORIGIN']; ?>/js/log.js"></script>
 <script>
-    Log.init(api,
-        document.getElementById("prev"),
-        document.getElementById("next"),
-        document.getElementById("page-display"),
-        <?php echo \App\Core\Context::get('total-workouts') ?>,
-        <?php echo \App\Core\Database\Database::config("pagination_default", \App\Core\Context::get('user')->fields["id"]) ?>
-    );
+    // No script
 </script>
