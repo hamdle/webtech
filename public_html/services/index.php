27ae8d24be0cb116a3ed624accf30be29c7d9d3c
Eric Jawaid Marty <ericmarty@protonmail.com>
2024-09-04 20:34:15 -0500

    Add entry point for services page

---
+++ public_html/services/index.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Core\Router;
+
+require_once dirname(__DIR__, 2) . "/autoload.php";
+
+$request = [
+    'method' => 'Page.login'
+];
+
+$router = new Router();
+$response = $router->handle($request);
+$response->send();
