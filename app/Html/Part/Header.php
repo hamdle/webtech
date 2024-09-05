ea5e4aa2a8118bdb5c656fa5c847c74cd5277ef4
Eric Jawaid Marty <ericmarty@protonmail.com>
2024-09-04 20:30:50 -0500

    Add configurable header logo and change menu links

--- app/Html/Part/Header.php
+++ app/Html/Part/Header.php
@@ -2,8 +2,8 @@
     <div class="p-navigation__row">
         <div class="p-navigation__banner">
             <div class="p-navigation__logo w-navigation__logo">
-                <a class="p-navigation__item w-navigation__item w-navigation__item--override" href="<?php echo $_ENV['ORIGIN']; ?>">
-                    <img class="p-navigation__logo-icon" src="<?php echo $_ENV['ORIGIN']; ?>/img/ejmtech-name-logo.png" alt="ejmtech.net logo">
+                <a class="p-navigation__item w-navigation__item w-navigation__item--<?php echo $_ENV['LOGO'] ?? 'name'; ?>" href="<?php echo $_ENV['ORIGIN']; ?>">
+                    <img class="p-navigation__logo-icon" src="<?php echo $_ENV['ORIGIN']; ?>/img/ejmtech-<?php echo $_ENV['LOGO'] ?? 'name'; ?>-logo.png" alt="ejmtech.net logo">
                 </a>
             </div>
             <a href="#navigation" class="p-navigation__toggle--open" title="menu">Menu</a>
@@ -13,46 +13,25 @@
             <span class="u-off-screen">
                 <a href="#main-content">Jump to main content</a>
             </span>
-            <?php if (\App\Core\Context::get('session')->isAuthenticated()) { ?>
-                <ul class="p-navigation__items">
-                    <li class="p-navigation__item <?php if (\App\Core\Utils\Helper::onPage("/home")) { ?> is-selected <?php } ?>">
-                        <a class="p-navigation__link" href="/home">
-                            Dashboard
-                        </a>
-                    </li>
-                    <ul class="p-navigation__items">
-                        <li class="p-navigation__item--dropdown-toggle <?php if (\App\Core\Utils\Helper::onPage("/workout/go") || \App\Core\Utils\Helper::onPage("/workout") || \App\Core\Utils\Helper::onPage("/workout/timer")) { ?> is-selected <?php } ?>" id="link-1">
-                            <a class="p-navigation__link" aria-controls="account-menu" aria-expanded="false" href="#">
-                                Workout
-                            </a>
-                            <ul class="p-navigation__dropdown--right" id="account-menu" aria-hidden="true">
-                                <li>
-                                    <a href="/workout" class="p-navigation__dropdown-item">View</a>
-                                </li>
-                                <li>
-                                    <a href="/workout/go" class="p-navigation__dropdown-item">New</a>
-                                </li>
-                                <li>
-                                    <a href="/workout/timer" class="p-navigation__dropdown-item">Timer</a>
-                                </li>
-                            </ul>
-                        </li>
-                    </ul>
-                    <li class="p-navigation__item <?php if (\App\Core\Utils\Helper::onPage("/takepicture")) { ?> is-selected <?php } ?>">
-                        <a class="p-navigation__link" href="/takepicture">
-                            Takepicture
-                        </a>
-                    </li>
-                </ul>
-            <?php } else { ?>
-                <ul class="p-navigation__items">
-                    <li class="p-navigation__item  <?php if (!\App\Core\Utils\Helper::onPage("/login")) { ?> is-selected <?php } ?>">
-                        <a class="p-navigation__link" href="/">
-                            Home
-                        </a>
-                    </li>
-                </ul>
-            <?php } ?>
+
+            <ul class="p-navigation__items">
+
+                <li class="p-navigation__item  <?php if (\App\Core\Utils\Helper::onPage("/services")) { ?> is-selected <?php } ?>">
+                    <a class="p-navigation__link" href="/services">
+                        Services
+                    </a>
+                </li>
+                <li class="p-navigation__item  <?php if (\App\Core\Utils\Helper::onPage("/about")) { ?> is-selected <?php } ?>">
+                    <a class="p-navigation__link" href="/about">
+                        About
+                    </a>
+                </li>
+                <li class="p-navigation__item  <?php if (\App\Core\Utils\Helper::onPage("/contact")) { ?> is-selected <?php } ?>">
+                    <a class="p-navigation__link" href="/contact">
+                        Contact
+                    </a>
+                </li>
+            </ul>
 
             <?php if (\App\Core\Context::get('session')->isAuthenticated()) { ?>
                 <ul class="p-navigation__items">
@@ -61,6 +40,9 @@
                             <?php echo \App\Core\Context::get('user')->first_name." ".\App\Core\Context::get('user')->last_name; ?>
                         </a>
                         <ul class="p-navigation__dropdown--right" id="account-menu" aria-hidden="true">
+                            <li>
+                                <a href="/home" class="p-navigation__dropdown-item">Dashboard</a>
+                            </li>
                             <li>
                                 <a href="/user" class="p-navigation__dropdown-item">Settings</a>
                             </li>
